0c34d603336e5ad705e26a15e1e764e64082ec76
Pratheeba0505 <pratheeba0505@gmail.com>
2024-02-26 16:41:13 -0600

    Mars rover
    Mars rover

--- rover.js
+++ rover.js
@@ -1,5 +1,51 @@
+const Message = require('./message.js');
+const Command = require('./command.js');
+
 class Rover {
    // Write code here!
-}
+   constructor(position)
+   {
+    
+      this.position = position;
+      this.mode = "NORMAL";
+      this.generatorWatts = 110;
+   }
+   receiveMessage(theMessage) {
+      let message = theMessage.name;
+      let results = [];
+  
+      for(let i = 0; i < theMessage.commands.length; i++) {
+        if(theMessage.commands[i].commandType === "MOVE") {
+          if(this.mode === "LOW_POWER") {
+            results.push({completed: false});
+          }else{
+            results.push({completed: true});
+            this.position = theMessage.commands[i].value;
+          }
+        }else if(theMessage.commands[i].commandType === "STATUS_CHECK") {
+          results.push({completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
+        }else if(theMessage.commands[i].commandType === "MODE_CHANGE") {
+          results.push({completed: true});
+          this.mode = theMessage.commands[i].value;
+        }else{
+          throw Error("Command Type undefinded.");
+        }
+      }
+  
+      return {message, results};
+    }
+  }
+ let commands = [
+    new Command('MOVE', 4321),
+    new Command('STATUS_CHECK'),
+    new Command('MODE_CHANGE', 'LOW_POWER'),
+    new Command('MOVE', 3579),
+    new Command('STATUS_CHECK')
+  ];
+  let message = new Message('Test message with three commands',commands);
+  let  rover = new Rover(100);
+  let response = rover.receiveMessage(message);
+  console.log(response);
+
 
 module.exports = Rover;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const Message = require('../message.js');
 const Command = require('../command.js');
 
@@ -5,5 +6,23 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+    it("throws error if name NOT passed into constructor",function ()  {
+
+        assert.throws(() => new Message(), { message: 'Name required' });
+    });
+
+    it("should set name via constructor", function()  {
+        const message = new Message("Alex");
+
+        assert.strictEqual(message.name, "Alex");
+    });
+
+    it("should contain commands passed into constructor as 2nd argument", function() {
+        const commands = [new Command('MODE_CHANGE', 'LOW_POWER')
+                        , new Command('STATUS_CHECK')];
+        const message = new Message("Alex", commands);
+
+        assert.strictEqual(message.commands, commands);
+    });
 
 });
